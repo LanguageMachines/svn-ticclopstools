7b77070ef9b59c59c777f65b93f4ae6553e4422c
sloot <sloot@12f355fe-0486-481a-ad91-c297ab22b4e3>
2012-09-26 13:16:48 +0000

    tag

--- src/hashfile.cxx
+++ src/hashfile.cxx
@@ -1,3 +1,30 @@
+/*
+  $Id$
+  $URL$
+
+  Copyright (c) 1998 - 2012
+ 
+  This file is part of ticclopstools
+
+  ticclopstools is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+
+  ticclopstools is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+  For questions and suggestions, see:
+      
+  or send mail to:
+      
+*/
+
 #include <string>
 #include <fstream>
 #include <map>
